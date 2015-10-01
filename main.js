176d64be011688ad73d8f85e003d8e9624b7f6b0
Wesley Aspinall <wra427@gmail.com>
2015-10-01 15:07:48 -0400

    answer 1 js

---
+++ main.js
@@ -0,0 +1,48 @@
+//IIFE
+(function () {
+
+
+///question 1 Show me how to calculate the average price of all items.
+console.log(items.length);
+
+// items.forEach(function (item) {
+//   console.log(item.price;)
+// });
+
+var prices = items.map (function (item) {
+  return item.price;
+
+});
+
+//adding items together
+
+var sum = prices.reduce( function (previous, next) {
+
+  return prev + next;
+
+});
+console.log(sum);
+
+var avg = sum/ items.length;
+console.log(avg);
+
+//convert to 2 decimal places
+var converted = avg.toFixed(2);
+console.log(converted);
+
+//convert to string
+
+var str = ' the average price is $' + converted;
+  console.log(str);
+
+  //make it show up on the page
+    // find our answer element
+
+  var answer1 = document.querySelector('#answer1');
+
+  //create a node from our above answer
+  var textNode = document.createTextNode(str);
+
+  answer1.appendChild(textNode);
+
+}());
