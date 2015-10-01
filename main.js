b7973eeb0bd7a4fc080c14f325e377aa2a6bd4c6
Wesley Aspinall <wra427@gmail.com>
2015-10-01 17:06:20 -0400

    still on question 2, getting an issue when displaying the answer in the index...

--- main.js
+++ main.js
@@ -1,48 +1,89 @@
-//IIFE
-(function () {
+// //IIFE
+// (function () {
 
 
 ///question 1 Show me how to calculate the average price of all items.
-console.log(items.length);
+// console.log(items.length);
 
 // items.forEach(function (item) {
 //   console.log(item.price;)
 // });
 
-var prices = items.map (function (item) {
-  return item.price;
+// var prices = items.map (function (item) {
+//   return item.price;
 
-});
+// });
 
-//adding items together
+// //adding items together
 
-var sum = prices.reduce( function (previous, next) {
+// var sum = prices.reduce( function (previous, next) {
 
-  return previous + next;
+//   return previous + next;
 
-});
-console.log(sum);
+// });
+// console.log(sum);
 
-var avg = sum/ items.length;
-console.log(avg);
+// var avg = sum/ items.length;
+// console.log(avg);
 
-//convert to 2 decimal places
-var converted = avg.toFixed(2);
-console.log(converted);
+// //convert to 2 decimal places
+// var converted = avg.toFixed(2);
+// console.log(converted);
 
-//convert to string
+// //convert to string
 
-var str = ' the average price is $' + converted;
-  console.log(str);
+// var str = ' the average price is $' + converted;
+//   console.log(str);
 
-  //make it show up on the page
-    // find our answer element
+//   //make it show up on the page
+//     // find our answer element
 
-  var answer1 = document.querySelector('#answer1');
+//   var answer1 = document.querySelector('#answer1');
 
-  //create a node from our above answer
-  var textNode = document.createTextNode(str);
+//   //create a node from our above answer
+//   var textNode = document.createTextNode(str);
 
-  answer1.appendChild(textNode);
+//   answer1.appendChild(textNode);
 
-}());
+////////////////////////////////////////
+
+items.forEach(function(item){
+  // console.log(item.price);
+
+
+  var prices = items.map( function (item){
+    return item.price;
+   })
+  });
+  
+    var merchList=[];
+  
+    var merchandise = items.filter(function (merch){
+
+     if (merch.price > 14.00 && merch.price < 18.00){
+      
+      merchList.push(merch.title);
+
+      console.log( merch.title); 
+      
+      return merch.title;
+
+
+      var answer2 = document.querySelector('#answer2');
+      
+      var textNode = document.createTextNode(merchList);
+
+      answer2.appendChild(textNode);
+
+
+    };
+
+    
+  });
+
+
+
+// }());
+
+
+///////////////////////////////////
