7ef785b99d4bf3858b779bde279ee67d7b0e5e7e
Wesley Aspinall <wra427@gmail.com>
2015-10-01 19:18:37 -0400

    completed assignment 2

--- main.js
+++ main.js
@@ -1,89 +1,73 @@
-// //IIFE
-// (function () {
+//IIFE
+(function () {
 
 
-///question 1 Show me how to calculate the average price of all items.
-// console.log(items.length);
+//question 1 Show me how to calculate the average price of all items.
+console.log(items.length);
 
 // items.forEach(function (item) {
 //   console.log(item.price;)
 // });
 
-// var prices = items.map (function (item) {
-//   return item.price;
+var prices = items.map (function (item) {
+  return item.price;
 
 // });
 
-// //adding items together
+//adding items together
 
-// var sum = prices.reduce( function (previous, next) {
+var sum = prices.reduce( function (previous, next) {
 
-//   return previous + next;
+  return previous + next;
 
-// });
-// console.log(sum);
+});
+console.log(sum);
 
-// var avg = sum/ items.length;
-// console.log(avg);
+var avg = sum/ items.length;
+console.log(avg);
 
-// //convert to 2 decimal places
-// var converted = avg.toFixed(2);
-// console.log(converted);
+//convert to 2 decimal places
+var converted = avg.toFixed(2);
+console.log(converted);
 
-// //convert to string
+//convert to string
 
-// var str = ' the average price is $' + converted;
-//   console.log(str);
+var str = ' the average price is $' + converted;
+  console.log(str);
 
-//   //make it show up on the page
-//     // find our answer element
+  //make it show up on the page
+    // find our answer element
 
-//   var answer1 = document.querySelector('#answer1');
+  var answer1 = document.querySelector('#answer1');
 
-//   //create a node from our above answer
-//   var textNode = document.createTextNode(str);
+  //create a node from our above answer
+  var textNode = document.createTextNode(str);
 
-//   answer1.appendChild(textNode);
+  answer1.appendChild(textNode);
+
+  
 
 ////////////////////////////////////////
 
-items.forEach(function(item){
-  // console.log(item.price);
+ var itemTitle=[];
 
+  var merchList=items.forEach(function(item){
 
-  var prices = items.map( function (item){
-    return item.price;
-   })
+      if(item.price > 14 && item.price < 18){
+
+        itemTitle.push(item.title);
+      }
   });
-  
-    var merchList=[];
-  
-    var merchandise = items.filter(function (merch){
+console.log(itemTitle);
 
-     if (merch.price > 14.00 && merch.price < 18.00){
-      
-      merchList.push(merch.title);
+var answer2= document.querySelector('#answer2');
 
-      console.log( merch.title); 
-      
-      return merch.title;
+var textNode= document.write(itemTitle.join(" <br> "));
+
+})
 
 
-      var answer2 = document.querySelector('#answer2');
-      
-      var textNode = document.createTextNode(merchList);
-
-      answer2.appendChild(textNode);
-
-
-    };
-
-    
-  });
-
-
-
-// }());
+}());
 
 
 ///////////////////////////////////
