9193268318258d7e7fcc517755bb8e700ebfb16d
Wesley Aspinall <wra427@gmail.com>
2015-10-01 23:15:12 -0400

    finished question 3, but there's a problem with the way the answers display in index.html... apparently the nodeappendChild is necessary for question 2... will work on tomorrow.

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ var str = ' The average price is $' + converted+".";
   answer1.appendChild(textNode);
 
 
-
+/////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////Question 2
 
 //how this works: we use a forEach() method to iterate
@@ -76,8 +76,43 @@ var answer2= document.querySelector('#answer2');
 
 var textNode= document.write(itemTitle.join(" <br> "));
 
-/////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////Question 3
+
+var price = items.forEach(function(item){
+  console.log(item.price);
+});
+
+var currency = items.forEach(function(item){
+  console.log(item.currency_code);
+})
+
+var euro = items.map(function(item){
+  if (item.currency_code !== "USD"){
+    return item.title +" "+"costs"+" "+"£"+item.price+".";
+  } else{
+    return false;
+  }
+})
+console.log(euro);
+
+ccArray = euro.filter(function(item){
+  return item !== false;
+})
+
+console.log(ccArray);
+// console.log(ccArray.join(''));
+
+var str= ccArray.join('');
+
+console.log(str);
+
+var answer3 = document.querySelector('#answer3');
+var textNode = document.createTextNode(str);
+
+answer3.appendChild(textNode);
 
 
+/////////////////////////////////////////////////////
 }());
 
