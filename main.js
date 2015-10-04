9d87af7e29fd1b6abf633b6c170cce7c9595bb81
Wesley Aspinall <wra427@gmail.com>
2015-10-03 21:14:30 -0400

    completed assignment

--- main.js
+++ main.js
@@ -79,6 +79,8 @@ document.getElementById( 'answer2' ).innerHTML = itemTitles[0] + "<br />" + item
 // ///////////////////////////////////////////////////////////////
 // ////////////////////////////////////////////////////Question 3
 
+
+//checking what prints for item.price and item.currency_code
 var price = items.forEach(function(item){
   console.log(item.price);
 });
@@ -87,6 +89,8 @@ var currency = items.forEach(function(item){
   console.log(item.currency_code);
 })
 
+//creating an array of items that do not hav USD as currency code 
+//and returning a string within that array
 var euro = items.map(function(item){
   if (item.currency_code !== "USD"){
     return item.title +" "+"costs"+" "+ "\xA3"+item.price+".";
@@ -97,6 +101,8 @@ var euro = items.map(function(item){
 
 console.log(euro);
 
+//fitlering out all the undefined objects within the array
+
 ccArray = euro.filter(function(item){
   return item !== false;
 })
@@ -104,10 +110,13 @@ ccArray = euro.filter(function(item){
 console.log(ccArray);
 // console.log(ccArray.join(''));
 
+//creating a string from the array
 var str= ccArray.join('');
 
 console.log(str);
 
+//getting answer to display on page by creating a node
+//and passing it the string
 var answer3 = document.querySelector('#answer3');
 var textNode = document.createTextNode(str);
 
@@ -117,6 +126,10 @@ answer3.appendChild(textNode);
 ///////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////Question 4
 
+
+// item.materials.indexOf("wood") ===>
+// NOT item.indexof.... bc...
+//We are dealing with OBJECTS within an array.
 var containsWood = items.filter(function(item){
 
   if ( item.materials.indexOf("wood") !== -1){
@@ -128,12 +141,19 @@ var containsWood = items.filter(function(item){
 
 console.log(containsWood);
 
+
+//creating an array and returning a string within
+//that array
+
 var wood = containsWood.map(function(item){
   return item.title+" "+"is made of wood.";
 });
 
 console.log(wood);
 
+//since we are displaying an array and a node
+//only accepts strings, we are forced to use
+//.innerHTML
 document.getElementById( 'answer4' ).innerHTML = 
 wood[0] + "<br />" + wood[1]+ "<br/>" + wood[2]+ "</br>"+ wood[3]+ "<br/>" + wood[4];
 
@@ -141,12 +161,18 @@ wood[0] + "<br />" + wood[1]+ "<br/>" + wood[2]+ "</br>"+ wood[3]+ "<br/>" + woo
 
 ///////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////Question 5
+
+//creating a string within an array that returns the item title
+//along with the number of materials it uses
 var foo=items.map(function(item){ 
  if(item.materials.length >= 8){
   return item.title+" "+"has"+" "+item.materials.length+" "+"materials:"
   }
 });
 
+//in order to format that answer correctly,
+//we created an additional array to display under the first one...
+// this array contains the list of materials.
 var bar=items.map(function(item){ 
  if(item.materials.length >= 8){
   return item.materials;
@@ -155,6 +181,8 @@ var bar=items.map(function(item){
 
 // console.log(foo);
 
+
+//filtering out the undefined objects
 var fooFilt=foo.filter(function(item){
  if ( item !== "undefined"){
     return item;
@@ -171,6 +199,8 @@ var barFilt=bar.filter(function(item){
 console.log(fooFilt);
 console.log(barFilt);
 
+//using inner html to format the answer correctly. used join to create a string
+//for the list of materials. 
 document.getElementById( 'answer5' ).innerHTML = 
 fooFilt[0]+ "<br/>"+"<br/>" + barFilt[0].join("<br />") 
 
@@ -180,16 +210,20 @@ fooFilt[1]+ "<br/>"+"<br/>" + barFilt[1].join("<br />");
 ///////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////Question 6
 
+
+//fitler the array for item.who_made === "i_did"
 var iDidFilter = items.filter(function(item){
    return item.who_made === "i_did";
 });
 
  console.log(iDidFilter.length);
 
+//creating string for the answer
  var str = iDidFilter.length +" "+ "were made by their sellers."
 
  console.log(str);
 
+//creating node to display answer on page
 var answer6 = document.querySelector('#answer6');
 var textNode = document.createTextNode(str);
 
